0c99bab7f67c2a1af6a1d00a0046499f51aa6305
irishatesting <braslavets.irisha@gmail.com>
2024-03-19 17:50:50 +0200

    change the IsNaN function - new version

--- isNaNFunction.js
+++ isNaNFunction.js
@@ -1,6 +1,27 @@
-function isNaNFunction (param) {
-    console.log(param !== param);
-}
-let param = "qwerty"/10;
+function isNaNFunction(value) {
+    const num = Number(value);
+    return num !== num;
+  }
 
-isNaNFunction (param);
+console.log("My function: " + isNaNFunction (NaN));
+console.log("isNaN: " + isNaN(NaN));
+
+console.log("My function: " + isNaNFunction ("20"));
+console.log("isNaN: " + isNaN("20"));
+
+console.log("My function: " + isNaNFunction ("qwerty" / 10));
+console.log("isNaN: " + isNaN("qwerty" / 10));
+
+console.log("My function: " + isNaNFunction (undefined));
+console.log("isNaN: " + isNaN(undefined));
+
+
+console.log("My function: " + isNaNFunction (20));
+console.log("isNaN: " + isNaN(20));
+
+
+console.log("My function: " + isNaNFunction (null));
+console.log("isNaN: " + isNaN(null));
+
+console.log("My function: " + isNaNFunction ("qwerty"));
+console.log("isNaN: " + isNaN("qwerty"));
